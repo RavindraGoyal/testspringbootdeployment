2855dca1542a7b8a2a6f6de702247fc116c7ec0e
RavindraGoyal <ravindragoyal10@gmail.com>
2025-03-08 16:05:12 +0530

    test project added

---
+++ src/main/java/com/testdeployment/test/controller/Hello.java
@@ -0,0 +1,20 @@
+package com.testdeployment.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public  String PrintHello(){
+        return "Hello";
+    }
+
+    @GetMapping("/name/{name}")
+    public String PrintName(@PathVariable String name){
+        return "Hello " + name;
+    }
+
+}
---
+++ src/test/java/com/testdeployment/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdeployment.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
